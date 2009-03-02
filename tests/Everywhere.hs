645e30b65b99cda70da85bba2ad593283a864a68
leather <leather@51733d7a-322c-11dd-8ca2-af992726123e>
2009-03-02 23:19:26 +0000

    Add tests to Everywhere.
    git-svn-id: https://subversion.cs.uu.nl/repos/project.dgp-haskell.libraries/EMGM/trunk@720 51733d7a-322c-11dd-8ca2-af992726123e

--- tests/Everywhere.hs
+++ tests/Everywhere.hs
@@ -1,17 +1,18 @@
 
 module Everywhere (tests) where
 
-import Test.HUnit
+import TTree
 import Generics.EMGM as G
 
+import Test.HUnit
 import Data.Char (toUpper, toLower)
 
 -----------------------------------------------------------------------------
 -- Utility functions
 -----------------------------------------------------------------------------
 
-test_e descr f_actual val f_expected = descr ~: (everywhere f_actual val) ~?= f_expected val
-test_e' descr f_actual val f_expected = descr ~: (everywhere' f_actual val) ~?= f_expected val
+test_e  descr f_actual val f_expected = descr ~: (G.everywhere  f_actual val) ~?= f_expected val
+test_e' descr f_actual val f_expected = descr ~: (G.everywhere' f_actual val) ~?= f_expected val
 
 -----------------------------------------------------------------------------
 -- Test functions and values
@@ -40,11 +41,20 @@ f_maybe_double :: Maybe Double -> Maybe Double
 f_maybe_double Nothing = Just 5.0
 f_maybe_double (Just d) = Just (d / 20.8)
 
-f_list_char :: String -> String
-f_list_char = G.map toLower
+f_list_char1 :: String -> String
+f_list_char1 = G.map toLower
+
+f_list_char2 :: String -> String
+f_list_char2 (c:cs) = []
+f_list_char2 []     = []
 
 f_unit :: () -> ()
 f_unit = id
+
+f_ttree1 :: TTree Int -> TTree Int
+f_ttree1 (L1 4)         = L1 7
+f_ttree1 (L2 5 (L1 4))  = L1 9
+f_ttree1 x              = x
 
 -----------------------------------------------------------------------------
 -- Test collection
@@ -65,7 +75,8 @@ tests =
        , test_e "Maybe Double(Double)" f_double (Just (-2e10)::Maybe Double) (G.map f_double)
        , test_e "Maybe Double(Maybe Double)" f_maybe_double (Just (-2e10)::Maybe Double) f_maybe_double
        , test_e "[Char](Char)" f_char "emgm" (G.map f_char)
-       , test_e "[Char]([Char])" f_list_char "EMGM" f_list_char
+       , test_e "cons to nil" f_list_char1 "EMGM" f_list_char1
+       , test_e "[Char]([Char])" f_list_char2 "EMGM" (const [])
        , test_e "()" f_unit () id
        , test_e "(Int,Float)" f_float (42::Int,1.5::Float) (G.bimap id f_float)
        , test_e "(,)" f_unit ((),()) id
@@ -74,6 +85,8 @@ tests =
        , test_e "(,,,,)" f_unit ((),(),(),(),()) id
        , test_e "(,,,,,)" f_unit ((),(),(),(),(),()) id
        , test_e "(,,,,,,)" f_unit ((),(),(),(),(),(),()) id
+       , test_e "TTree1" f_ttree1 (L1 4) f_ttree1
+       , test_e "TTree2" f_ttree1 (L2 (5::Int) (L1 4)) (const (L2 5 (L1 7)))
        ]
 
     , "Everywhere'" ~:
@@ -88,7 +101,8 @@ tests =
        , test_e' "Maybe Double(Double)" f_double (Just (-2e10)::Maybe Double) (G.map f_double)
        , test_e' "Maybe Double(Maybe Double)" f_maybe_double (Just (-2e10)::Maybe Double) f_maybe_double
        , test_e' "[Char](Char)" f_char "emgm" (G.map f_char)
-       , test_e' "[Char]([Char])" f_list_char "EMGM" f_list_char
+       , test_e' "[Char]([Char])" f_list_char1 "EMGM" f_list_char1
+       , test_e' "[Char]([Char])" f_list_char2 "EMGM" (const [])
        , test_e' "()" f_unit () id
        , test_e' "(Int,Float)" f_float (42::Int,1.5::Float) (G.bimap id f_float)
        , test_e' "(,)" f_unit ((),()) id
@@ -97,6 +111,8 @@ tests =
        , test_e' "(,,,,)" f_unit ((),(),(),(),()) id
        , test_e' "(,,,,,)" f_unit ((),(),(),(),(),()) id
        , test_e' "(,,,,,,)" f_unit ((),(),(),(),(),(),()) id
+       , test_e' "TTree1" f_ttree1 (L1 4) f_ttree1
+       , test_e' "TTree2" f_ttree1 (L2 (5::Int) (L1 4)) (const (L1 9))
        ]
 
     ]
